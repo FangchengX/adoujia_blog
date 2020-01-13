ae9616403860b509d5ad857ad9dde956952807a9
fangcheng <kq6441@163.com>
2020-01-13 19:53:33 +0800

    add sql/config

--- sql/create_vv_00.00_xfc.sql
+++ sql/create_vv_00.00_xfc.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `blog` default charset utf8 COLLATE utf8_general_ci;
+create DATABASE IF NOT EXISTS `blog` default charset utf8 COLLATE utf8_general_ci;
 create table `blog`.`user` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `uuid` varchar(50) NOT NULL COMMENT 'uuid',
@@ -10,4 +10,21 @@ create table `blog`.`user` (
  `updateTime` bigint(20) NULL COMMENT '更新时间',
  PRIMARY KEY (`id`),
  KEY `idx_uuid` (`uuid`)
-)Engine=Innodb charset=utf8mb4 COMMENT '用户';
+)Engine=Innodb charset=utf8mb4 COMMENT='用户';
+
+create table `blog`.`role` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `role` varchar(20) NOT NULL COMMENT '角色名',
+ PRIMARY KEY (`id`),
+ KEY `idx_role` (`role`)
+)Engine=Innodb charset=utf8mb4 COMMENT='角色';
+
+insert into `blog`.`role` (`id`, `role`) values (1, 'USER');
+
+create table `blog`.`user_role` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `userId` int(11) NOT NULL COMMENT '',
+ `roleId` int(11) NOT NULL COMMENT '',
+ PRIMARY KEY (`id`),
+ KEY `idx_userId` (`userId`)
+)Engine=Innodb charset=utf8mb4 COMMENT='角色';
